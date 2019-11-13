67a12290cb88ea09f1162f422f18b67eca1de5c6
hi9a <k1959792@kadai.jp>
2019-11-13 23:41:24 +0900

    Update dl.php

--- dl.php
+++ dl.php
@@ -7,72 +7,83 @@ const SEASON_MONTH_SET = [
     ['2018', '201901'],
     ['2018', '201902'],
     ['2018', '201903'],
+    ['2019', '201908'],
+    ['2019', '201909'],
+    ['2019', '201910'],
+    ['2019', '201911'],
+    ['2019', '201912'],
+    ['2019', '202001'],
+    ['2019', '202002'],
 ];
 const MATCH_URL = 'https://tleague.jp/match/';
 const TEAM = '琉球';
-const CSV = '/tmp/output.csv';
-const CSV_TITLE = ['date', 'team', 'team score', 'opponent score', 'opponent', 'home/away', 'arena'];
+const CSV = 'php://output';
+const CSV_HEADER = ['date', 'team', 'team-score', 'opponent-score', 'opponent-team', 'home-away', 'arena'];
 
 function csv_records($resource, $url) {
     $csv_records = [];
 
     curl_setopt($resource, CURLOPT_HEADER, false);
-    // http://php.net/manual/en/function.curl-exec.php
-    // if the CURLOPT_RETURNTRANSFER option is set, it will return the result on success, FALSE on failure. 
     curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($resource, CURLOPT_BINARYTRANSFER, true);
     curl_setopt($resource, CURLOPT_URL, $url);
-    curl_setopt($resource, CURLOPT_SSLVERSION,1); 
+    curl_setopt($resource, CURLOPT_SSLVERSION,1);
     $html = curl_exec($resource);
     $dom = new DOMDocument();
 
-    // 取得htmlが適切でないらしく下記warningが出力される。
-    // PHP Warning:  DOMDocument::loadHTML(): htmlParseEntityRef: no name in Entity
-    // 出したくないなら
-    // libxml_use_internal_errors(true);
+    libxml_use_internal_errors(true);
     $dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
 
     $xpath = new DOMXPath($dom);
     $match_men = $xpath->query('.//li[@class="match-men"]');
 
     foreach ($match_men as $match) {
+        $a_record = [];
+
         $c_home = $xpath->query('.//div[@class="cell-home"] /a', $match);
         $c_away = $xpath->query('.//div[@class="cell-away"] /a', $match);
 
         $home = $c_home->item(0)->textContent;
         $away = $c_away->item(0)->textContent;
-        if (!in_array(TEAM, [$home, $away])) {
+        if (!in_array(TEAM, [$home, $away], true)) {
             continue;
         }
 
         $c_score = $xpath->query('.//div[@class="cell-result"] /div /strong', $match);
-        $score = $c_score->item(0)->textContent;
-        $scores = explode(' ', $score); // [home score, '-'(constant string value), away score]
+        $score = '';
+        $scores = ['', ''];
+        if ($c_score->item(0)) {
+            $score = $c_score->item(0)->textContent;
+            $scores = explode(' - ', $score);
+        }
 
         $c_date = $xpath->query('.//div[@class="cell-date"]', $match);
-        $date = $c_date->item(0)->textContent;
-
+        $c_arena = $xpath->query('.//div[@class="cell-arena"] /a', $match);
         $home_game = TEAM === $home;
 
-        $team           = $home_game ? $home      : $away;
-        $opponent       = $home_game ? $away      : $home;
-        $team_score     = $home_game ? $scores[0] : $scores[2];
-        $opponent_score = $home_game ? $scores[2] : $score[0];
-        $homeaway       = $home_game ? 'home'     : 'away'; 
+        $a_record['date']           = $c_date->item(0)->textContent;
+        $a_record['team']           = $home_game ? $home      : $away;
+        $a_record['team-score']     = $home_game ? $scores[0] : $scores[1];
+        $a_record['opponent-score'] = $home_game ? $scores[1] : $scores[0];
+        $a_record['opponent-team']  = $home_game ? $away      : $home;
+        $a_record['home-away']      = $home_game ? 'home'     : 'away';
+        $a_record['arena']          = $c_arena->item(0)->textContent;
 
-        $c_arena = $xpath->query('.//div[@class="cell-arena"] /a', $match);
-        $arena = $c_arena->item(0)->textContent;
+        $csv_record = [];
+        foreach (CSV_HEADER as $column) {
+            $csv_record[$column] = $a_record[$column];
+        }
 
-        $csv_records[] = [$date, $team, $team_score, $opponent_score, $opponent, $homeaway, $arena];
+        $csv_records[] = $csv_record;
     }
     return $csv_records;
 }
 
-////////////////////////////////
+// ====
 // main
-////////////////////////////////
+// ====
 $resource = curl_init();
-$csv_records = [CSV_TITLE];
+$csv_records = [CSV_HEADER];
 $csv_file = fopen(CSV, 'w');
 $season_month_set = SEASON_MONTH_SET;
 
